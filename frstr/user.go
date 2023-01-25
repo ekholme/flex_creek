542329816a5267ae7409d629c12203196eeac347
Eric Ekholm <eric.ekholm@gmail.com>
2023-01-25 06:54:27 -0500

    start userservice methods

--- frstr/user.go
+++ frstr/user.go
@@ -1,4 +1,76 @@
 package frstr
 
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+	flexcreek "github.com/ekholme/flex_creek"
+)
+
 //TODO
 //implement UserService methods
+
+const userColl = "users"
+
+type userService struct {
+	Client *firestore.Client
+}
+
+func NewUserService(client *firestore.Client) flexcreek.UserService {
+	return &userService{
+		Client: client,
+	}
+}
+
+// add methods
+func (us userService) CreateUser(ctx context.Context, u *flexcreek.User) error {
+	_, _, err := us.Client.Collection(userColl).Add(ctx, u)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (us userService) GetAllUsers(ctx context.Context) ([]*flexcreek.User, error) {
+
+	var users []*flexcreek.User
+
+	docs, err := us.Client.Collection(userColl).Documents(ctx).GetAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range docs {
+		var user *flexcreek.User
+
+		v.DataTo(&user)
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (us userService) GetUserByID(ctx context.Context, id string) (*flexcreek.User, error) {
+	return nil, nil
+}
+
+func (us userService) UpdateUser(ctx context.Context, id string, u *flexcreek.User) (*flexcreek.User, error) {
+	return nil, nil
+}
+
+func (us userService) DeleteUser(ctx context.Context, id string) error {
+	return nil
+}
+
+func (us userService) GetUserFavoriteWods(ctx context.Context, id string) ([]*flexcreek.Wod, error) {
+	return nil, nil
+}
+
+// this probably isn't correct
+func (us userService) Login(ctx context.Context, username string, pw string) (*flexcreek.User, error) {
+	return nil, nil
+}
--- user.go
+++ user.go
@@ -15,6 +15,8 @@ type UserService interface {
 	CreateUser(ctx context.Context, u *User) error
 	GetAllUsers(ctx context.Context) ([]*User, error)
 	GetUserByID(ctx context.Context, id string) (*User, error)
-	UpdateUser(ctx context.Context, id string) (*User, error)
+	UpdateUser(ctx context.Context, id string, u *User) (*User, error)
 	DeleteUser(ctx context.Context, id string) error
+	GetUserFavoriteWods(ctx context.Context, id string) ([]*Wod, error)
+	Login(ctx context.Context, username string, pw string) (*User, error) //this isn't correct but will fix later
 }
